bcd7e07daef5828d12ecc06d3f711a49eb5a3ee8
scottharris86 <scottharrisdev@gmail.com>
2020-04-27 10:23:55 -0400

    Initial Commit

---
+++ Clamtown Crossfit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Clamtown Crossfit
+//
+//  Created by scott harris on 4/27/20.
+//  Copyright © 2020 scott harris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
