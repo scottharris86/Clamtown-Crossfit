de652b41dee01531cfdb37c572769bfd13abdfd0
scottharris86 <scottharrisdev@gmail.com>
2020-04-27 18:18:45 -0400

    header view (not completely sold on it yet..)

--- Clamtown Crossfit/View Controllers/MainNavigationController.swift
+++ Clamtown Crossfit/View Controllers/MainNavigationController.swift
@@ -9,5 +9,33 @@
 import UIKit
 
 class MainNavigationController: UINavigationController {
+    let navBarAppearance = UINavigationBarAppearance()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navBarAppearance.configureWithOpaqueBackground()
+        navBarAppearance.backgroundColor = .white
+        navigationBar.prefersLargeTitles = true
+        navigationBar.standardAppearance = navBarAppearance
+        navigationBar.scrollEdgeAppearance = navBarAppearance
+        navigationBar.compactAppearance = navBarAppearance
+        let logoImageView = UIImageView()
+        logoImageView.image = UIImage(named: "logo-clamtown-crossfit")
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        navigationBar.addSubview(logoImageView)
+        
+        navigationItem.titleView = logoImageView
+//        self.navigationItem.titleView = logoImageView
+//        navigationBar.addSubview(logoImageView)
+//        logoImageView.widthAnchor.constraint(equalToConstant: 32).isActive = true
+        logoImageView.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        logoImageView.leadingAnchor.constraint(equalTo: navigationBar.leadingAnchor, constant: 16).isActive = true
+        logoImageView.trailingAnchor.constraint(equalTo: navigationBar.trailingAnchor, constant: -60).isActive = true
+        logoImageView.bottomAnchor.constraint(equalTo: navigationBar.bottomAnchor, constant: -8).isActive = true
+        
+    }
+    
+    
     
 }
