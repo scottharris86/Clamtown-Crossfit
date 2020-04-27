5c139c43f0f2071c2acadfa0dd413227a93bc39a
scottharris86 <scottharrisdev@gmail.com>
2020-04-27 17:35:27 -0400

    basic workout feed cell static data for now

--- Clamtown Crossfit/View Controllers/WorkoutsFeedCollectionViewController.swift
+++ Clamtown Crossfit/View Controllers/WorkoutsFeedCollectionViewController.swift
@@ -9,9 +9,37 @@
 import UIKit
 
 class WorkoutsFeedCollectionViewController: UICollectionViewController {
+    let cellCount = 5
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.backgroundColor = .white
+        collectionView.register(WorkoutFeedCell.self, forCellWithReuseIdentifier: "FeedCell")
         
     }
     
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cellCount
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FeedCell", for: indexPath)
+        
+        return cell
+        
+    }
+    
+    
+    
+}
+
+extension WorkoutsFeedCollectionViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.bounds.width - 4
+        
+        return CGSize(width: width, height: width - 60)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 5
+    }
 }
---
+++ Clamtown Crossfit/Views/Cells/WorkoutFeedCell.swift
@@ -0,0 +1,103 @@
+//
+//  WorkoutFeedCell.swift
+//  Clamtown Crossfit
+//
+//  Created by scott harris on 4/27/20.
+//  Copyright © 2020 scott harris. All rights reserved.
+//
+
+import UIKit
+
+class WorkoutFeedCell: UICollectionViewCell {
+    let playerPreviewView = UIImageView()
+    let coachImageView = UIImageView()
+    let dateLabel = UILabel()
+    let titleLabel = UILabel()
+    let subtitleLabel = UILabel()
+    
+    let separatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(red: 230 / 255, green: 230 / 255, blue: 230 / 255, alpha: 1)
+        
+        return view
+    }()
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupSubviews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Create Cell Programmatically Only")
+    }
+    
+    private func setupSubviews() {
+        configurePlayerView()
+        configureCoachImageAvatar()
+        configureSeparatorViewConstraints()
+        configureTitleLabel()
+        configureSubtitleLabel()
+        
+    }
+    
+    private func configurePlayerView() {
+        playerPreviewView.image = UIImage(named: "bobs_video_thumbnail")
+        contentView.addSubview(playerPreviewView)
+        playerPreviewView.translatesAutoresizingMaskIntoConstraints = false
+        playerPreviewView.topAnchor.constraint(equalTo: topAnchor, constant: 8).isActive = true
+        playerPreviewView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8).isActive = true
+        playerPreviewView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -8).isActive = true
+        playerPreviewView.heightAnchor.constraint(equalTo: playerPreviewView.widthAnchor, multiplier: 9.0/16.0).isActive = true
+
+    }
+    
+    private func configureCoachImageAvatar() {
+        coachImageView.contentMode = .scaleAspectFill
+        coachImageView.image = UIImage(named: "sara")?.withRenderingMode(.alwaysOriginal)
+        contentView.addSubview(coachImageView)
+        coachImageView.translatesAutoresizingMaskIntoConstraints = false
+        coachImageView.topAnchor.constraint(equalTo: playerPreviewView.bottomAnchor, constant: 16).isActive = true
+        coachImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16).isActive = true
+        coachImageView.widthAnchor.constraint(equalToConstant: 44).isActive = true
+        coachImageView.heightAnchor.constraint(equalTo: coachImageView.widthAnchor).isActive = true
+        coachImageView.layer.cornerRadius = 22
+        coachImageView.layer.masksToBounds = true
+        
+    }
+    
+    private func configureSeparatorViewConstraints() {
+        addSubview(separatorView)
+        separatorView.translatesAutoresizingMaskIntoConstraints = false
+        separatorView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
+        separatorView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
+        separatorView.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        separatorView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+        
+    }
+    
+    private func configureTitleLabel() {
+        addSubview(titleLabel)
+        titleLabel.text = "CLAMTOWN HOME WORKOUT - 4/26"
+        titleLabel.font = .systemFont(ofSize: 15)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.leadingAnchor.constraint(equalTo: coachImageView.trailingAnchor, constant: 8).isActive = true
+        titleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16).isActive = true
+        titleLabel.centerYAnchor.constraint(equalTo: coachImageView.centerYAnchor).isActive = true
+        
+    }
+    
+    private func configureSubtitleLabel() {
+        addSubview(subtitleLabel)
+        subtitleLabel.text = "Coach: Sarah                     Time: 30:00"
+        subtitleLabel.font = .systemFont(ofSize: 12, weight: .bold)
+        subtitleLabel.textColor = UIColor.init(white: 0.5, alpha: 0.7)
+        subtitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        subtitleLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor).isActive = true
+        subtitleLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16).isActive = true
+        subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 4).isActive = true
+    }
+    
+    
+    
+}
